c6ed76ec88c91716bd80039e7df4db2d77a99ead
alysonabril <alysonabril@pursuit.org>
2018-10-05 16:45:18 -0400

    first commit

---
+++ testNumber5000/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testNumber5000
+//
+//  Created by Alyson Abril on 10/5/18.
+//  Copyright © 2018 Alyson Abril. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
